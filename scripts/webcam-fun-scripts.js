03a924a0c8b2c5c371fcaac6ca39d556e9e7e6f9
Kyle Williams <williams.kyleg@gmail.com>
2020-07-13 18:45:19 -0400

    video feed, canvas draw, working on red

--- scripts/webcam-fun-scripts.js
+++ scripts/webcam-fun-scripts.js
@@ -3,3 +3,58 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+function getVideo() {
+    navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+    .then(localMediaStream => {
+        console.log(localMediaStream);
+        // video.src = window.URL.createObjectURL(localMediaStream);
+        video.srcObject = localMediaStream;
+        video.play();
+    })
+    .catch(err => {
+        console.log('There has been an error. ', err);
+    });
+}
+
+function paintToCanvas() {
+    const width = video.videoWidth;
+    const height = video.videoHeight;
+    canvas.width = width;
+    canvas.height = height;
+
+    return setInterval(() => {
+        ctx.drawImage(video, 0, 0, width, height);
+        // Take out the pixels.
+        let pixels = ctx.getImageData(0, 0, width, height);
+        // Do something with the pixels.
+        pixels = redEffect(pixels);
+        // Return the pixels after manipulation.
+        ctx.putImageData(pixels, 0, 0);
+    }, 16);
+}
+
+function takePhoto() {
+    snap.currentTime = 0;
+    snap.play();
+
+    const data = canvas.toDataURL('image/jpeg');
+    const link = document.createElement('a');
+    link.href = data;
+    link.setAttribute('download', 'handsome');
+    link.innerHTML = `<img src="${data}" alt="Handsome Man" />`
+    strip.insertBefore(link, strip.firstChild);
+}
+
+function redEffect(pixels) {
+    for(let i = 0; i < pixels.length; i += 4) {
+        pixels.data[i] = pixels.data[i + 0] + 100; // R
+        pixels.data[i + 1] = pixels.data[i + 1] - 50; // G
+        pixels.data[i + 2] = pixels.data[i + 2] * 0.5; // B
+    }
+    return pixels;
+}
+
+getVideo();
+
+video.addEventListener('canplay', paintToCanvas);
